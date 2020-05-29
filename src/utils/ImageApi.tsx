71a2af1b5ad28c669d521e60d8edf0150a9b45bd
Paolo <fedeli.paolol@gmail.com.com>
2020-05-29 10:26:49 +0200

    react-create-app on Heroku

--- src/utils/ImageApi.tsx
+++ src/utils/ImageApi.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const getImages = async () => {
   const page = Math.floor(Math.random() * 20 + 1);
-  const urlImages = `https://api.unsplash.com/search/photos?page=${page}&query=Landscape&client_id=${process.env.REACT_APP_UNPLASH_KEY}`;
+  const urlImages = `https://api.unsplash.com/search/photos?page=${page}&query=Landscape&client_id=${lbxNnSa7rMUFhzJTFptbMgRne30G84iKZszBhOjnAcA}`;
   ;
 
   const res = await axios.get(urlImages);
--- src/utils/colorApi.tsx
+++ src/utils/colorApi.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const getColor = async () => {
  
-  const urlImages = `https://www.colr.org/json/colors/random/8`;
+  const urlImages = `http://www.colr.org/json/colors/random/8`;
   const res = await axios.get(urlImages);
     const colors = res.data    
 
